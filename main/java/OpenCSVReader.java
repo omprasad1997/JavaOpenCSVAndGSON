020c7a6d1b9ab0364322079488af7367b90d37e7
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-09 19:16:24 +0530

    Updated : Read all records from CSV file at once

--- main/java/OpenCSVReader.java
+++ main/java/OpenCSVReader.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "./users.csv";
@@ -12,12 +13,12 @@ public class OpenCSVReader {
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CSVReader csvReader = new CSVReader(reader);
         ){
-        String[] nextRecord;
-        while((nextRecord = csvReader.readNext()) != null){
-                System.out.println("Name : " + nextRecord[0]);
-                System.out.println("Email : " + nextRecord[1]);
-                System.out.println("Phone : " + nextRecord[2]);
-                System.out.println("Country : " + nextRecord[3]);
+        List<String[]> records = csvReader.readAll();
+        for(String[] record : records){
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
                 System.out.println("===========================");
             }
         }
